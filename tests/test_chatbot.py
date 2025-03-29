c07ea172b3078fed93f3d80ca9df71575ba9764f
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-03-28 21:16:10 -0500

    first test_chabot modification: Added a unittest for get account number function.

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -12,7 +12,50 @@ Example:
 
 __author__ = "COMP-1327 Faculty"
 __version__ = "1.0.2025"
-
+import unittest
 from unittest import TestCase, main
 from unittest.mock import patch
 from src.chatbot import ACCOUNTS, VALID_TASKS
+from src.chatbot import get_account_number
+
+class Testchatbot(unittest.TestCase):
+# test if the get account number raises a typeerror.
+    def test_get_account_number_raise_type_error(self):
+        with patch('builtins.input') as mock_input:
+            # Arrange
+            mock_input.side_effect = ["knmskmdn"]
+            expected = "Account number must be an int type."
+
+            # Act
+            with self.assertRaises(TypeError) as context:
+                get_account_number()
+
+            # Assert
+            self.assertEqual(expected, str(context.exception))
+            
+# test if get account number raises a valueerror if account don't exist.
+    def test_get_account_number_raise_value_error(self):
+        with patch('builtins.input') as mock_input:
+            # Arrange
+            mock_input.side_effect = ["123654"]
+            expected = "Account number entered does not exist."
+
+            # Act
+            with self.assertRaises(ValueError) as context:
+                get_account_number()
+
+            # Assert
+            self.assertEqual(expected, str(context.exception))
+# test if get account number returns a valid account number.
+    def test_get_account_number(self):
+        with patch('builtins.input') as mock_input:
+            # Arrange
+            mock_input.side_effect = ["123456"]
+            expected = 123456
+
+            # Act
+            actual = get_account_number()
+
+            # Assert
+            self.assertEqual(expected, actual)
+            
